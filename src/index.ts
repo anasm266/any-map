314e9e80d2191e1be88fea88a2646f2962bcf5b5
Anas <kingzcopz266@gmail.com>
2026-04-22 23:22:54 -0700

    fix: buildScanOptions compatible with exactOptionalPropertyTypes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export type { AnySource, GreedyCoverPick, ScanSummary, SourceKind, SourceRanked } from "./types.js";
-export { classifyScan, runFullScan } from "./analyzer/run-scan.js";
+export { buildScanOptions, classifyScan, runFullScan } from "./analyzer/run-scan.js";
 export type { FullScanResult, ScanOptions, ScanResult } from "./analyzer/run-scan.js";
 export {
   filterSources,
